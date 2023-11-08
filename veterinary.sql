793b7f3d47e92e58d5b4c0f57aba3e3328167cb6
man-gg <143564063+man-gg@users.noreply.github.com>
2023-11-08 13:26:13 +0800

    Update veterinary.sql

--- veterinary.sql
+++ veterinary.sql
@@ -4,7 +4,6 @@ CREATE DATABASE veterinary;
     ownerid INT PRIMARY KEY, 
     ofirstname VARCHAR(50) NOT NULL,
     phone VARCHAR(15) NOT NULL, 
-    phone VARCHAR(15) NOT NULL,
     olastname VARCHAR(50) NOT NULL, 
     address VARCHAR(15) NOT NULL, 
     email VARCHAR(100) NOT NULL );

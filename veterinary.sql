47a35ea611dc94430e9522f9c71e31e36b24bc00
Glycel <glycel.virtucio@gmail.com>
2023-11-08 12:36:30 +0800

    feat/list-species-catered

--- veterinary.sql
+++ veterinary.sql
@@ -113,14 +113,5 @@ ADD COLUMN registereddate DATE;
 ALTER TABLE invoices
 RENAME COLUMN paymentdate TO paymenttime;
 
-DELETE FROM invoices
-WHERE appointid = (
-	SELECT appointid 
-	FROM appointments 
-	WHERE animalid = (
-		SELECT animalid 
-		FROM animals WHERE name = 'Simba'));
-
-UPDATE doctors
-SET dlastname = 'Reyes-Gonzales'
-WHERE dfirstname = 'Dr. Sofia';
+SELECT DISTINCT species
+FROM animals;
